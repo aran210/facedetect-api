47cfd13d9cb03e1ca587d69099dfa426964458f0
Aran <aran.ubhi@me.com>
2020-07-21 22:13:00 +0100

    host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image.js');
 const db = knex ({
     client: 'pg',					
     connection: {
-      host : '127.0.0.1',
-      user : 'aran',
-      password : '',
-      database : 'facedetect-db'
+      connectString : 'process.env.DATABASE_URL',
+      ssl: true
     }
 });
 
