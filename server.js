de873dc2cab94d30bd1ac76eec41a16e8170b01c
Aran <aran.ubhi@me.com>
2020-07-21 22:42:07 +0100

    re-adding 3000 for local tests

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`running smoothly on port ${process.env.PORT}`);
 });
